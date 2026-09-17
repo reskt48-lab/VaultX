cf1b44d4c9a9c121f30ec85d258e7feda1c2e8ee
Res Aja... <reskt48@gmail.com>
2026-09-17 08:13:06 +0700

    Delete run.bat

--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ================================================
-echo        TASKLY - Aplikasi Manajemen Tugas
-echo ================================================
-echo.
-echo Menjalankan aplikasi...
-echo.
-python main.py
-pause
